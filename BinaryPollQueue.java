24eb3cba16de42cceaac352d0b64d12b1a4d75f0
catrone3 <catrone3@att.net>
2017-11-16 14:31:02 -0500

    lab 8

---
+++ BinaryPollQueue.java
@@ -0,0 +1,67 @@
+package lab8;
+
+/* WRITE YOUR NAME HERE */
+
+
+/* Implements a FIFO-Priority Queue using two LIFO Stacks.
+ * offer: add an element to this queue, return true if element is added successfully, return false otherwise
+ * priorityPoll: removes and returns the element with the highest priority.
+ * elementPoll:  removes and returns the element at the front of the queue.
+ * peek: returns the element at the front of the queue.              
+ */
+
+import java.util.*;
+
+public class BinaryPollQueue<E extends Comparable<E>> { // Do not change this line
+    
+    /* Two LIFO Stacks to act as the container of this BinaryPollQueue. DO NOT CHANGE!!! */
+    private Stack<E> stk1;
+    private Stack<E> stk2;
+    
+    public boolean offer(E e){
+        stk1.push(e);
+        return true;
+    }//offer
+    
+    public E elementPoll(){
+        while(!stk1.isEmpty()){
+            stk2.push(stk1.pop());
+        }//while
+        E old = stk2.pop();
+        while(!stk2.isEmpty()){
+            stk1.push(stk2.pop());
+        }//while
+        return old;
+    }//element poll
+    
+    public E peek(){
+        while(!stk1.isEmpty()){
+            stk2.push(stk1.pop());
+        }//while
+        E old = stk2.peek();
+        while(!stk2.isEmpty()){
+            stk1.push(stk2.pop());
+        }//while
+        return old;
+    }//elementpeek
+    
+    
+    public E priorityPoll(){
+        E maxPriority = stk1.peek();
+        while(!stk1.isEmpty()){
+            if(stk1.peek().compareTo(maxPriority)>=0){
+                maxPriority = stk2.pop();
+            }//if
+            stk2.push(stk1.pop());
+        }//while
+        while(!stk2.isEmpty()){
+            if(maxPriority.equals(stk2.peek())){
+                maxPriority = stk2.pop();
+            }//if
+            stk1.push(stk2.pop());
+        }//while
+        return maxPriority;
+    }//elementPoll
+}
+
+
